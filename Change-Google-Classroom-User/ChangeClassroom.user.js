47ddeaeff3c9ed6ac6f50bc54688b189c5f035d5
ITGGOT-Emrik-Ostling <emrik.ostling@elev.ga.ntig.se>
2020-04-05 23:25:25 +0200

    Improved detection mechanic

--- Change-Google-Classroom-User/ChangeClassroom.user.js
+++ Change-Google-Classroom-User/ChangeClassroom.user.js
@@ -2,12 +2,12 @@
 // @name        Change Google Classroom User
 // @namespace   Change Google Classroom User by C4illin
 // @description Swithces between /u/0 & /u/1 on google classroom automatically.
-// @match       *://classroom.google.com/*
+// @match       https://classroom.google.com/*
 // @run-at      document-idle
 // @grant       none
 // @author      C4illin <C@illin.cf>
 // @oujs:author C4illin
-// @version     1.1.4
+// @version     1.1.5
 // @downloadURL https://github.com/C4illin/Userscripts/raw/master/Change-Google-Classroom-User/ChangeClassroom.user.js
 // @updateURL   https://github.com/C4illin/Userscripts/raw/master/Change-Google-Classroom-User/ChangeClassroom.user.js
 // @homepageURL https://github.com/C4illin/Userscripts/tree/master/Change-Google-Classroom-User
@@ -15,19 +15,28 @@
 // @copyright   2018-2019, C4illin (https://github.com/C4illin)
 // ==/UserScript==
 
-function checkuser(){
-  if ((document.getElementsByClassName("tCD5Mc").length != 0) || (document.getElementsByClassName("FKF6mc").length != 0)) {
+;(function() {
+  let redirected = false
+  setInterval(() => {
+    if (!redirected) {
+      if ((document.getElementsByClassName("Lqr2e").length != 0) && (window.location.pathname.length < 7)) {
+        switchUser()
+      }
+      if ((document.getElementsByClassName("tCD5Mc").length != 0) && (window.location.pathname.length > 7)) {
+        switchUser()
+      }
+    } 
+  }, 250)
+  
+  function switchUser() {
+    redirected = true
+    let user
     if (window.location.pathname.substring(3,4) == 0) {
-      var user = 1
+      user = 1
     } else {
       user = 0
     }
-    var newlink = "https://classroom.google.com/u/"+ user + window.location.pathname.substring(4) + window.location.search + window.location.hash
+    let newlink = "https://classroom.google.com/u/"+ user + window.location.pathname.substring(4) + window.location.search + window.location.hash
     window.location.replace(newlink)
   }
-}
-
-checkuser()
-setTimeout(function(){
-  checkuser()
-}, 3000)
+})()
