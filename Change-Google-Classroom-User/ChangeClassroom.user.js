4513356cb6de3146812e096713e52807cd981880
ITGGOT-Emrik-Ostling <emrik.ostling@elev.ga.ntig.se>
2020-04-05 23:30:50 +0200

    Updated Copyright information

--- Change-Google-Classroom-User/ChangeClassroom.user.js
+++ Change-Google-Classroom-User/ChangeClassroom.user.js
@@ -12,7 +12,7 @@
 // @updateURL   https://github.com/C4illin/Userscripts/raw/master/Change-Google-Classroom-User/ChangeClassroom.user.js
 // @homepageURL https://github.com/C4illin/Userscripts/tree/master/Change-Google-Classroom-User
 // @license     MIT; https://opensource.org/licenses/MIT
-// @copyright   2018-2019, C4illin (https://github.com/C4illin)
+// @copyright   2018-2020, C4illin (https://github.com/C4illin)
 // ==/UserScript==
 
 (function() {
