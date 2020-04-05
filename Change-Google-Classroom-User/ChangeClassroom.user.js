655fb334b667eadc8633e124b586f0a4f0847f2c
ITGGOT-Emrik-Ostling <emrik.ostling@elev.ga.ntig.se>
2020-04-05 23:30:14 +0200

    1.2.0 release

--- Change-Google-Classroom-User/ChangeClassroom.user.js
+++ Change-Google-Classroom-User/ChangeClassroom.user.js
@@ -7,7 +7,7 @@
 // @grant       none
 // @author      C4illin <C@illin.cf>
 // @oujs:author C4illin
-// @version     1.1.5
+// @version     1.2.0
 // @downloadURL https://github.com/C4illin/Userscripts/raw/master/Change-Google-Classroom-User/ChangeClassroom.user.js
 // @updateURL   https://github.com/C4illin/Userscripts/raw/master/Change-Google-Classroom-User/ChangeClassroom.user.js
 // @homepageURL https://github.com/C4illin/Userscripts/tree/master/Change-Google-Classroom-User
@@ -15,21 +15,18 @@
 // @copyright   2018-2019, C4illin (https://github.com/C4illin)
 // ==/UserScript==
 
-;(function() {
-  let redirected = false
-  setInterval(() => {
-    if (!redirected) {
-      if ((document.getElementsByClassName("Lqr2e").length != 0) && (window.location.pathname.length < 7)) {
-        switchUser()
-      }
-      if ((document.getElementsByClassName("tCD5Mc").length != 0) && (window.location.pathname.length > 7)) {
-        switchUser()
-      }
-    } 
+(function() {
+  let refresh = setInterval(() => {
+    if ((document.getElementsByClassName("Lqr2e").length != 0) && (window.location.pathname.length < 7)) {
+      switchUser()
+    }
+    if ((document.getElementsByClassName("tCD5Mc").length != 0) && (window.location.pathname.length > 7)) {
+      switchUser()
+    }
   }, 250)
   
   function switchUser() {
-    redirected = true
+    clearInterval(refresh)
     let user
     if (window.location.pathname.substring(3,4) == 0) {
       user = 1
