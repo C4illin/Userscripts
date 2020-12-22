344bf2287ac6079b75514a815ba1e93264cdfde3
C4illin <emrik.ostling@gmail.com>
2020-12-22 13:52:04 +0100

    added dk

--- Prisjakt-Redirect-Skiper/PrisjaktRedirectSkiper.user.js
+++ Prisjakt-Redirect-Skiper/PrisjaktRedirectSkiper.user.js
@@ -12,11 +12,12 @@
 // @match       *://*.ledenicheur.fr/redirect.php?ftgid=*
 // @match       *://*.pagomeno.it/redirect.php?ftgid=*
 // @match       *://*.hintaopas.fi/redirect.php?ftgid=*
+// @match       *://*.prisjagt.dk/redirect.php?ftgid=*
 // @run-at      document-start
 // @grant       none
 // @author      C4illin <C@illin.cf>
 // @oujs:author C4illin
-// @version     1.2.1
+// @version     1.2.2
 // @license     MIT; https://opensource.org/licenses/MIT
 // @copyright   2018, C4illin (https://github.com/C4illin)
 // ==/UserScript==
