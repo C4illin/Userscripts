e3c9d1ed17dce2093684735b538e6df7bd92a938
C4illin <emrik.ostling@gmail.com>
2021-04-27 19:28:27 +0200

    fixed problem with unreleased products

--- APK-Systembolaget/apk.systembolaget.user.js
+++ APK-Systembolaget/apk.systembolaget.user.js
@@ -3,7 +3,7 @@
 // @namespace   APK Systembolaget by C4illin
 // @match       https://www.systembolaget.se/produkt/*
 // @grant       none
-// @version     1.2
+// @version     1.3
 // @author      C4illin
 // @description Adds APK value to product pages
 // @run-at      document-idle
@@ -12,7 +12,7 @@
 let refresh = setInterval(() => {
   let main = document.querySelector(".col-md-7.offset-md-1")?.children
   let alkohol = main[2].firstChild.firstChild?.children[2]?.children[0]?.innerText
-  let price = main[2]?.children[1].firstChild?.innerText.replace(" ","").replace(":-","").replace(":",".")
+  let price = main[2]?.children[1]?.firstChild?.innerText.replace(" ","").replace(":-","").replace(":",".")
   let volume = main[2].firstChild.firstChild?.children[1]?.children[0]?.innerText
   let apk = Math.round((alkohol?.split(' ')[0].replace(",",".")*volume?.split(' ')[0]/100/price + Number.EPSILON) * 1000) / 1000
   //console.log("APK: " + apk)
