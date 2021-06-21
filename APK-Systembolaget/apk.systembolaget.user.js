7b7e62d26f521316fa1ee0559656c41634a0a50d
C4illin <emrik.ostling@gmail.com>
2021-06-21 13:50:35 +0200

    alkohol fria funkar igen

--- APK-Systembolaget/apk.systembolaget.user.js
+++ APK-Systembolaget/apk.systembolaget.user.js
@@ -5,7 +5,7 @@
 // @icon        https://raw.githubusercontent.com/alkolist/alkolist.github.io/main/icon/android-chrome-512x512.png
 // @match       https://www.systembolaget.se/produkt/*
 // @grant       none
-// @version     1.6
+// @version     1.7
 // @author      C4illin <e@mrik.ga>
 // @homepageURL https://alkolist.github.io/
 // @run-at      document-idle
@@ -16,21 +16,22 @@ GM_info.script.homepageURL = "https://alkolist.github.io/"
 let refresh = setInterval(() => {
   let main = document.querySelector(".col-md-7.offset-md-1")?.children
   if (main) {
-    let offset = 0
-    if (main.length == 5) {
-      offset = 1
+    //let offset = 0
+    let alkohol = main[2]?.firstChild?.firstChild?.children[2]?.children[0]?.innerText
+    if (alkohol == null) {
+      alkohol = "0 %"
     }
-    let alkohol = main[2+offset]?.firstChild?.firstChild?.children[2]?.children[0]?.innerText
+    
     if (alkohol.endsWith("g/l")) {
       alkohol = "0 %"
     }
 
-    let price = Number(main[2+offset]?.children[1]?.firstChild?.innerText.replace(" ","").replace(":-","").replace(":","."))
-    let volume = main[2+offset]?.firstChild?.firstChild?.children[1]?.children[0]?.innerText
+    let price = Number(main[2]?.children[1]?.firstChild?.innerText.replace(" ","").replace(":-","").replace(":","."))
+    let volume = main[2]?.firstChild?.firstChild?.children[1]?.children[0]?.innerText
     let apk = Math.round((alkohol?.split(' ')[0].replace(",",".")*volume?.split(' ')[0]/100/price + Number.EPSILON) * 1000) / 1000
     if (!apk.isNaN){
       clearInterval(refresh)
-      let maind = main[2+offset].firstChild.firstChild
+      let maind = main[2].firstChild.firstChild
       let apkdisplay = maind.children[1].cloneNode()
       apkdisplay.innerText = ("APK " + apk).replace(".",",")
       maind.append(apkdisplay)
